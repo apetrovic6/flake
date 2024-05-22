6e2eff42e92c3629f636ceb092228c804299c6b3
apetrovic6 <petrovicante6@gmail.com>
2024-05-22 16:36:23 +0200

    switch to the new nvidia driver

--- base/gpu/nvidia/default.nix
+++ base/gpu/nvidia/default.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ...}:
+{ config, pkgs, lib, ...}:
 {
 
   hardware.opengl = {
@@ -15,6 +15,18 @@
     
     powerManagement.finegrained = false;
 
-    package = config.boot.kernelPackages.nvidiaPackages.beta;
+    package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
+  version = "555.42.02";
+  sha256_64bit = "sha256-k7cI3ZDlKp4mT46jMkLaIrc2YUx1lh1wj/J4SVSHWyk=";
+  sha256_aarch64 = lib.fakeSha256;
+  openSha256 = "sha256-rtDxQjClJ+gyrCLvdZlT56YyHQ4sbaL+d5tL4L4VfkA=";
+  settingsSha256 = "sha256-rtDxQjClJ+gyrCLvdZlT56YyHQ4sbaL+d5tL4L4VfkA=";
+  persistencedSha256 = lib.fakeSha256;
+
+};
+
   };
+
+
+
 }
