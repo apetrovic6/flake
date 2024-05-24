13656607d674a2b6b4d7b5b676f6811b73a310db
apetrovic6 <petrovicante6@gmail.com>
2024-05-24 21:02:43 +0200

    change GC settings

--- base/config/default.nix
+++ base/config/default.nix
@@ -8,6 +8,8 @@
   nix.gc = {
     automatic = true;
     dates = "weekly";
-    options = "--delete-older-than 30d";
+    options = "--delete-older-than 1w";
   };
+
+  nix.settings.auto-optimise-store = true;
 }
