80d48f4d7c4dd22acb8a788055ae4187b27cffc9
apetrovic6 <petrovicante6@gmail.com>
2024-05-25 12:09:54 +0200

    update alacritty config

--- home/default.nix
+++ home/default.nix
@@ -53,13 +53,26 @@
     settings = {
       env.TERM = "xterm-256color";
       font = {
-        size = 12;
+        size = 13;
+	normal =  {
+          family = "FiraCodeNerdFont";
+	  style = "Retina";
+	};
+
+};
+     window = {
+	padding =  {
+	  x = 15;
+	  y = 15;
+	};
+
+	};
    #     draw_bold_text_with_bright_colors = true;
-      };
-      scrolling.multiplier = 5;
+scrolling.multiplier = 5;
       selection.save_to_clipboard = true;
-    };
-  };
+
+      };
+          };
 
 #   programs.neovim = {
 #    enable = true;
@@ -105,6 +118,7 @@ home.file = {
      ".ideavimrc" = {
       source = ./ideavimrc;
     };
+
   };
 
 
