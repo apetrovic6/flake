877347289c5aa9bbfaf3be5b783f755ac09c913f
apetrovic6 <petrovicante6@gmail.com>
2024-06-01 14:56:44 +0200

    stylix changes

--- base/stylix/default.nix
+++ base/stylix/default.nix
@@ -3,7 +3,8 @@
 {
 # stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-medium.yaml";
 
-  stylix.image = /home/apetrovic/Pictures/Wallpapers/38.jpg;
+  #stylix.image = /home/apetrovic/Pictures/Wallpapers/38.jpg;
+  stylix.image = /home/apetrovic/Pictures/Wallpapers/2.jpg;
 
   stylix.polarity = "dark";
 
@@ -20,8 +21,6 @@
     desktop = 1.0;
     popups = 1.0;
   };
-
-
 
   stylix.targets.plymouth.enable = true;
 
