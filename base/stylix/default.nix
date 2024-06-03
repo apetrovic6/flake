dbf72cc578f6e52bedac063f24f4779b059aedfc
apetrovic6 <petrovicante6@gmail.com>
2024-06-03 16:20:06 +0200

    change font sizes

--- base/stylix/default.nix
+++ base/stylix/default.nix
@@ -1,21 +1,21 @@
-{ config, pkgs, ...}:
+{ config, pkgs, ... }:
 
 {
-# stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-medium.yaml";
+  # stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-medium.yaml";
 
   #stylix.image = /home/apetrovic/Pictures/Wallpapers/38.jpg;
-  stylix.image = /home/apetrovic/Pictures/Wallpapers/2.jpg;
+  stylix.image = /home/apetrovic/Pictures/Wallpapers/25.jpg;
 
   stylix.polarity = "dark";
 
-  stylix.fonts.sizes =  {
-    applications = 13;
-    terminal = 15;
-    desktop = 14;
+  stylix.fonts.sizes = {
+    applications = 12;
+    terminal = 14;
+    desktop = 12;
     popups = 10;
   };
 
-  stylix.opacity =  {
+  stylix.opacity = {
     applications = 1.0;
     terminal = 0.8;
     desktop = 1.0;
